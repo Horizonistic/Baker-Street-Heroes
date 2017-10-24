62e887ef58136e5e344046c6970d7f9a1383c437
Horizonstic <horizonistic@gmail.com>
2017-10-23 19:44:30 -0700

    Actually added class for resource loading

--- inc/resources.h
+++ inc/resources.h
@@ -5,4 +5,21 @@
 #ifndef GAME_RESOURCES_H
 #define GAME_RESOURCES_H
 
+#include "oxygine-framework.h"
+
+namespace bsh
+{
+    class Res
+    {
+    public:
+        static oxygine::Resources ui;
+        static oxygine::Resources terrain;
+    
+        Res() {};
+        static void loadUI();
+        static void loadTerrain();
+        static void free();
+    };
+}
+
 #endif //GAME_RESOURCES_H
--- src/resources.cpp
+++ src/resources.cpp
@@ -2,3 +2,23 @@
 // Created by Jacob on 10/23/17.
 //
 
+#include "../inc/resources.h"
+
+oxygine::Resources bsh::Res::ui;
+oxygine::Resources bsh::Res::terrain;
+
+void bsh::Res::loadUI()
+{
+    bsh::Res::ui.loadXML("ui.xml");
+}
+
+void bsh::Res::loadTerrain()
+{
+    bsh::Res::terrain.loadXML("world.xml");
+}
+
+void bsh::Res::free()
+{
+    bsh::Res::ui.free();
+    bsh::Res::terrain.free();
+}
