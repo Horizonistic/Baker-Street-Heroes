6b709881d649a49adf43e23c82425c527382e570
Horizonstic <horizonistic@gmail.com>
2017-10-27 16:48:42 -0700

    Removed Doxygen files from Box2D

--- Box2D/doxygen/html/search/functions_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['postsolve',['PostSolve',['../classb2ContactListener.html#acd58ec96f7569b95eec65b8ca3f8013d',1,'b2ContactListener']]],
-  ['presolve',['PreSolve',['../classb2ContactListener.html#a416f85eb45a1099053402b15a19a7de0',1,'b2ContactListener']]]
-];
